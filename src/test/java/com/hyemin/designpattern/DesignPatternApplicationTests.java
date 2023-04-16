8b63790edc8d26459567ab5061b01e5b3c74e4c6
박혜민 <hyemin@ktown4u.com>
2023-04-16 20:10:25 +0900

    init

---
+++ src/test/java/com/hyemin/designpattern/DesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyemin.designpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
